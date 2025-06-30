230ca907ca820e79c2070549420353c03ad5304b
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-06-30 23:35:13 +0530

    refactor: update IMessage interface and schema to use single attachment type and improve structure

--- models/Messages.ts
+++ models/Messages.ts
@@ -14,13 +14,22 @@ export type Attachment = {
   contentType?: string;
 };
 
+export type ForkedVersion = {
+  version: number;
+  messageId: mongoose.Types.ObjectId;
+};
+
 // Main Message Interface
 export interface IMessage {
   _id?: mongoose.Types.ObjectId; // Optional for new documents
   chatId: mongoose.Types.ObjectId;
   role: Role;
   content: string;
-  attachments?: Attachment[]; // Optional, can be undefined
+  attachment: Attachment; // Optional, can be undefined
+  forkMessageGroupId?: mongoose.Types.ObjectId;
+  version?: number;
+  active?: boolean;
+  forkedVersions?: ForkedVersion[];
   createdAt?: Date;
   updatedAt?: Date;
 }
@@ -41,17 +50,32 @@ const messageSchema = new Schema<IMessage>(
       type: String,
       required: true,
     },
-    attachments: {
+    attachment: {
+      type: { name: String, url: String, contentType: String },
+    },
+    forkMessageGroupId: {
+      type: Schema.Types.ObjectId,
+      default: null,
+    },
+    version: {
+      type: Number,
+      default: 1,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    forkedVersions: {
       type: [
         {
-          name: String,
-          url: String,
-          contentType: String,
+          version: Number,
+          messageId: Schema.Types.ObjectId,
         },
       ],
       default: undefined,
     },
   },
+
   { timestamps: true }
 );
 
