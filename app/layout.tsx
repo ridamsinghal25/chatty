a08f16be87d3c60cd1a3642b4826f75c138ce08a
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-06-30 23:35:48 +0530

    feat: enhance layout with MessageProvider and Toaster, and update middleware for public route handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { type Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Sidebar } from "@/components/Sidebar";
 import { Header } from "@/components/Header";
+import { Toaster } from "react-hot-toast";
+import { MessageProvider } from "./context/MessageContext/MessageContextProvider";
+import { ChatProvider } from "./context/ChatContext/ChatContextProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,13 +36,20 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} bg-[#212121] text-white`}
         >
           <SidebarProvider>
-            <Sidebar />
-            <div className="flex-1">
-              <div className="sticky top-0 z-40 bg-background">
-                <Header />
-              </div>
-              <main>{children}</main>
-            </div>
+            <ChatProvider>
+              <MessageProvider>
+                <SignedIn>
+                  <Sidebar />
+                </SignedIn>
+                <div className="flex-1">
+                  <div className="sticky top-0 z-40 bg-background">
+                    <Header />
+                  </div>
+                  <main>{children}</main>
+                  <Toaster />
+                </div>
+              </MessageProvider>
+            </ChatProvider>
           </SidebarProvider>
         </body>
       </html>
--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhook(.*)",
+  "/",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
